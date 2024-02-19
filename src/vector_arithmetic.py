7ecd7838503dc7e5e24644574dd4d0021d202758
Chee Wai Wong <academicsnotes@gmail.com>
2024-02-19 21:45:21 +0800

    commit new scripts

---
+++ src/vector_arithmetic.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([20,40,60])
+b = np.array([10,20,30])
+c = np.array([5.10,15,20])
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+scalar=2
+list_a = [10,11,12,13,14,15]
+list_as_array = np.array(list_a)
+print(list_a)
+print(list_as_array)
+print(scalar * list_a)
+print(scalar * list_as_array)
